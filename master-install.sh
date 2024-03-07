7ce0b62997397466a00cef8566ee7def424d4b6d
Rizal Hidayat <rizalhidayat180499@gmail.com>
2024-03-07 20:00:30 +0700

    chore: update packages repository kubernetes

--- master-install.sh
+++ master-install.sh
@@ -12,8 +12,8 @@ sudo apt -y upgrade
 sudo apt -y install curl apt-transport-https ca-certificates curl software-properties-common
 
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt update
 sudo apt -y install vim git curl wget kubelet=1.28.2-00 kubeadm=1.28.2-00 kubectl=1.28.2-00
