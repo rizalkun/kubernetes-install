34d67b6c86271f6b8ded9a59ce0c9c7c0cab5eed
Rizal Hidayat <rizalhidayat180499@gmail.com>
2024-03-07 20:06:16 +0700

    refactor: remove spesific version install

--- master-install.sh
+++ master-install.sh
@@ -16,7 +16,7 @@ curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --
 sudo echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt update
-sudo apt -y install vim git curl wget kubelet=1.28.2-00 kubeadm=1.28.2-00 kubectl=1.28.2-00
+sudo apt -y install vim git curl wget kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
